b028848aaf2da2fc2d51257a29192e3098645e52
peoray <peoray@yahoo.com>
2024-01-16 14:26:54 +0100

    feat: Add axios API helper class

--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,31 @@
-console.log('API')
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
+
+export class AnchorCore {
+  public request: AxiosInstance
+  constructor(apiKey: string) {
+    this.request = axios.create({
+      baseURL: 'http://localhost:3000',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        'x-anchor-key': apiKey,
+      },
+    })
+  }
+
+  public async get(url: string, params: any = {}): Promise<AxiosResponse> {
+    return this.request.get(url, { params })
+  }
+
+  public async post(url: string, data: any = {}): Promise<AxiosResponse> {
+    return this.request.post(url, data)
+  }
+
+  public async patch(url: string, data: any = {}): Promise<AxiosResponse> {
+    return this.request.patch(url, data)
+  }
+
+  public async delete(url: string): Promise<AxiosResponse> {
+    return this.request.delete(url)
+  }
+}
